a8db999d96de42a020106f0db86b644be1ddfb26
jneisen <joseph@neisen.xyz>
2025-03-22 15:43:54 -0400

    added a system for sustaining hits

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
 var playerMovement : Vector2
-@export var groundSpeed = 10.0
-@export var airSpeed = 5.0
-@export var jumpHeight = 10.0
-@export var fallSpeedDecreaser = 0.9
+@export var maxSpeed = 350.0
+@export var groundSpeed = 20.0
+@export var airSpeed = 15.0
+@export var airFriction = 5.0
+@export var friction = 15.0
+@export var jumpHeight = 350.0
+@export var fallSpeedDecreaser = 0.6
+var hitstunTimer = 0
 var health : float
 var lives = 3
 
@@ -19,13 +23,10 @@ var grounded : bool
 
 var facingLeft = false
 
-var attackCode : int
-var specialCode : int
-
 
 #func _ready() -> void:
 	
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	left = Input.is_action_pressed("Left")
 	right = Input.is_action_pressed("Right")
 	up = Input.is_action_pressed("Jump")
@@ -39,27 +40,36 @@ func _process(delta: float) -> void:
 	if(right && facingLeft):
 		facingLeft = false
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
+	if(hitstunTimer > 0):
+		hitstunTimer -= _delta
+		return
 	grounded = is_on_floor()
 	# moving left and right on the ground
-	if(left && grounded):
+	if(left && grounded && playerMovement.x > -maxSpeed):
 		playerMovement.x -= groundSpeed
-	if(right && grounded):
+	if(right && grounded && playerMovement.x < maxSpeed):
 		playerMovement.x += groundSpeed
-	if(left && !grounded):
+	if(left && !grounded && playerMovement.x > -maxSpeed):
 		playerMovement.x -= airSpeed
-	if(right && !grounded):
+	if(right && !grounded && playerMovement.x < maxSpeed):
 		playerMovement.x += airSpeed
+	if(!left && !right && grounded):
+		playerMovement.x -= sign(playerMovement.x) * friction
+	if(!left && !right && !grounded):
+		playerMovement.x -= sign(playerMovement.x) * airFriction
 	
 	# jumping + gravity
 	if(up && grounded):
-		playerMovement.y -= jumpHeight
+		playerMovement.y = -jumpHeight
 		grounded = false
+	elif(grounded):
+		playerMovement.y = 0
 	elif(up && !grounded && playerMovement.y < 0):
 		# decrease gravity slightly if moving upwards and pressing jump
-		playerMovement.y += get_gravity().y * fallSpeedDecreaser
+		playerMovement.y += get_gravity().y * fallSpeedDecreaser * _delta
 	elif(!grounded):
-		playerMovement.y += get_gravity().y
+		playerMovement.y += get_gravity().y * _delta
 	
 	# start a move
 	if(attack || special):
@@ -80,6 +90,10 @@ func _physics_process(delta: float) -> void:
 			attackDirection(attackCode)
 		if(special):
 			specialDirection(attackCode)
+	
+	# move and slide to complete character movements for this frame
+	velocity = playerMovement
+	move_and_slide()
 
 # functions that execute an attack
 func attackDirection(attackCode):
@@ -90,3 +104,9 @@ func attackDirection(attackCode):
 func specialDirection(attackCode):
 	if(attackCode == "sideLeft"):
 		return
+
+func hitSelf(knockback, hitstun, damage):
+	playerMovement = knockback
+	health += damage
+	hitstunTimer = hitstun
+	

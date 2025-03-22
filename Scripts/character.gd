ebb405d0afcf6ea105ca7a7b16110b92f9c1a4d8
jneisen <joseph@neisen.xyz>
2025-03-22 14:37:48 -0400

    setting up base character movement

---
+++ Scripts/character.gd
@@ -0,0 +1,8 @@
+extends CharacterBody2D
+
+var playerMovement : Vector2
+@export var groundSpeed = 10.0
+@export var airSpeed = 10.0
+@export var jumpHeight = 10.0
+var health : float
+var lives = 3

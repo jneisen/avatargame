212a38642e0474bc84681e0b38d18c2096db81ed
kndym <kevin.nday12@gmail.com>
2025-03-23 04:39:27 -0400

    play test time

--- Scripts/end_screen.gd
+++ Scripts/end_screen.gd
@@ -7,10 +7,10 @@ func setWinner(player_1_loss) -> void:
 		$VBoxContainer/Label.text="Player 1 Wins"
 
 func _on_continue_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/character_options.tscn") 
+	get_tree().change_scene_to_file("res://Scenes/stage.tscn") 
 	queue_free()
 	
 func _on_end_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/stage.tscn") 
+	get_tree().change_scene_to_file("res://Scenes/end.tscn") 
 	queue_free()
 	
